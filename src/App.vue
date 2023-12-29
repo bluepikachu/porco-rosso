e986788d3944dcb8a1de70c31846dbff1e8936bc
Bluepikachu <vivianbefree@gmail.com>
2023-12-29 22:04:09 +0800

    feat: 基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,83 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+// import Dialog from './components/Dialog.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section class="m-6 border border-black/10 h-full">
+    <section class="mx-auto w-full max-w-4xl px-4 sm:px-6 lg:px-8">
+      <h1 class="py-20">真正的观点交锋——大师对谈之：《红猪》完成纪念，宫本茂VS宫崎骏
+      </h1>
+
+      <Dialog type="left"></Dialog>
+    
+      <div class="flex italic">
+        <p class="text-2xl">”</p>
+        <p class="py-4 content">
+          本次采访刊载于1992年的《Fami-Maga》杂志（ Fami 通之前日本最大的游戏杂志，已停刊）。在这次采访进行的时候，宫崎骏和吉卜力工作室刚刚完成了一部鸿篇巨制《红猪》。宫本茂和当时的 Fami-Maga 编辑一起看了这部电影，并于之后参观了吉卜力工作室与宫崎骏对谈。这篇文章，就收录了两人对谈的部分内容。在《红猪》即将于国内上映之时，我特此翻译，廖表纪念。
+        </p>
+        <div class="flex items-end text-2xl">“</div>
+      </div>
+    
+    <section class="flex flex-col gap-4 py-4">
+      <div class="flex gap-2">
+        <div class="flex justify-center items-center w-12 h-12 bg-black/5 rounded-full">gbm</div>
+        <div class="max-w-xl bg-black/5 p-2 rounded-xl border border-black/5 content">我太喜欢这部电影了。很难解释为何我会喜欢这部电影，也很难明确说出我到底喜欢它的哪个部分，然而这部电影对我来说毫无尿点。从头到尾，我只需要去看而不用太多地去思考。我喜欢简单的镜头，比如一艘飞船停泊在水面上。</div>
+      </div>
+    
+      <div class="flex gap-2 justify-end">
+        <div class="max-w-xl bg-black/5 p-2 rounded-xl border border-black/5 content">是的，从某种程度上说，这是一部简单的电影。</div>
+        <div class="flex justify-center items-center w-12 h-12 bg-black/5 rounded-full">gqj</div>
+      </div>
+    
+      <div class="flex gap-2">
+        <div class="flex justify-center items-center w-12 h-12 bg-black/5 rounded-full">gbm</div>
+        <div class="max-w-xl bg-black/5 p-2 rounded-xl border border-black/5">我忍不住以游戏开发者的角度去看里面的狗斗场景。那种速度感是在游戏里很难去重现的。</div>
+      </div>
+    
+      <div class="flex gap-2 justify-end">
+        <div class="max-w-xl bg-black/5 p-2 rounded-xl border border-black/5">我没有为这部电影准备剧本。另外，在这部电影中，我没有什么想特别表达来强调的信息或者中心思想。所以拍这部电影实际上是一个非常艰难且骇人的经历，因为我不确定我是否能给观众一个满意的交代。</div>
+        <div class="flex justify-center items-center w-12 h-12 bg-black/5 rounded-full">gqj</div>
+      </div>
+    
+      </section>
+    
+      <section class="flex flex-col gap-2 py-4">
+        <p>补充</p>
+        <div class="flex note gap-2"> 
+          <div>
+            1.
+          </div>
+          <div>译者注：这篇文章没有标题党，真的是宫本茂VS宫崎骏，两人观点的激烈碰撞让场面一度失控。然后就是宫崎骏的单方面输出和宫本茂的答不上来。然而大师始终是大师，在这样的观点交锋中，宫本茂竟然学到了宫崎骏创作哲学的真谛，并且学以致用，创造了塞尔达开发者们奋斗一生的“空气感”。</div>
+        </div>
+        <div class="flex note gap-2"> 
+          <div>
+            2.
+          </div>
+          <div>原文地址：<a href="https://www.bilibili.com/read/cv24202714/?spm_id_from=333.1007.0.0">https://www.bilibili.com/read/cv24202714/?spm_id_from=333.1007.0.0</a></div>
+        </div>
+        <div class="flex note gap-2"> 
+          <div>
+            3.
+          </div>
+          <div>红猪简介</div>
+        </div>
+        <p class="info">Design & Develop by Bluepikachu</p>
+      </section>
+     
+    </section>
+     
+   
+  </section>
+
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+html{
+  background-color:#fff9f1;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+body{
+  background-color:#fff9f1;
 }
 </style>
